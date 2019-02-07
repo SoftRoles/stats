a73d5dc87884ef6064bb205276c922b2bc36f79a
Hüseyin YİĞİT (BILGEM) <huseyin.yigit@tbtk.gov.tr>
2019-02-07 15:18:27 +0300

    Initial commit

---
+++ index.py
@@ -0,0 +1,31 @@
+import psutil
+import json
+from datetime import datetime
+import time
+
+psutil.cpu_times()
+
+while True:
+    cpuPercent = 100.0 - \
+        float(str(psutil.cpu_times_percent(interval=1.0).idle))
+    memPercent = psutil.virtual_memory().percent
+    message = {
+        'cpu': cpuPercent,
+        'mem': memPercent,
+        'disks': [],
+        'users': []
+    }
+    for partition in psutil.disk_partitions():
+      try:
+        message['disks'].append(
+            {'drive': partition.device, 'usage': psutil.disk_usage(partition.device).percent})
+      except:
+        pass
+
+    for user in psutil.users():
+        now = datetime.timestamp(datetime.now())
+        userLogon = user.started
+        passed = now - userLogon
+        message['users'].append(
+            {'name': user.name, 'logon': datetime.fromtimestamp(passed).strftime("%H:%M:%S")})
+    print(str(json.dumps(message)))
