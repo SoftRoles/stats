0909ac277a8f15f038df942d6174d7e3bed5cb49
Hüseyin Yiğit <yigit.hsyn@gmail.com>
2019-02-14 11:21:53 +0300

    socket broadcasting tested

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 // modules
 //=============================================================================
 const { spawn } = require('child_process')
-const path = require('path')
 const argparse = require('argparse').ArgumentParser
+const websocket = require('ws')
 
 //-------------------------------------
 // arguments
@@ -15,13 +15,31 @@ const argParser = new argparse({
 argParser.addArgument(['-p', '--port'], { help: 'TCP port', defaultValue: '5000' })
 const args = argParser.parseArgs()
 
+//-------------------------------------
+// websocket httpServer
+//-------------------------------------
+const server = new websocket.Server({ port: Number(args.port) })
+server.on('connection', (client, req)=>{
+  client.isAlive = true
+  // client.spawn = spawn('python', ['index.py']);
+  // console.log(ws)
+  // server.clients.forEach(client=>{
+    // console.log(client)
+  // })
+  // console.log(server.clients)
+  // console.log(req.headers['x-forwarded-for'].split(/\s*,\s*/)[0])
+  console.log(req.connection.remoteAddress)
+})
+
 //=============================================================================
 // child process
 //=============================================================================
 const python = spawn('python', ['index.py']);
 
 python.stdout.on('data', (data) => {
-  console.log(`${data}`);
+  server.clients.forEach(client=>{
+    if(client.readyState === websocket.OPEN) client.send(`${data}`)
+  })
 });
 
 python.stderr.on('data', (data) => {
@@ -31,3 +49,7 @@ python.stderr.on('data', (data) => {
 python.on('close', (code) => {
   console.log(`python exited with ${code}`);
 });
+
+//=============================================================================
+// start socket
+//=============================================================================
